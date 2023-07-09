421138bba99fc91af3c5260a7e0a3021a6af560a
LAPTOP-Q8PH4MAI\PC <solivervmazo.jobs@gmail.com>
2023-07-10 01:38:27 +0800

    first commit

---
+++ src/components/about/AboutClients.vue
@@ -0,0 +1,33 @@
+<script>
+import AboutClientSingle from '@/components/about/AboutClientSingle.vue';
+import clients from '@/data/clients.js';
+
+export default {
+	components: {
+		AboutClientSingle,
+	},
+	data: () => {
+		return {
+			clientsHeading: 'Some of the brands I have worked with',
+			clients,
+		};
+	},
+};
+</script>
+
+<template>
+	<div class="mt-10 sm:mt-20">
+		<p
+			class="font-general-medium text-2xl sm:text-3xl text-primary-dark dark:text-primary-light"
+		>
+			{{ clientsHeading }}
+		</p>
+		<div class="grid grid-cols-2 sm:grid-cols-4 mt-10 sm:mt-14 gap-2">
+			<AboutClientSingle
+				v-for="client in clients"
+				:key="client.id"
+				:client="client"
+			/>
+		</div>
+	</div>
+</template>
---
+++ src/data/projects.js
@@ -0,0 +1,43 @@
+// Projects data.
+// Note: Here we are getting data from a js file, but in a different project I'll be fetching these projects from some srt of APIs.
+
+const projects = [
+	{
+		id: 1,
+		title: 'Google Health Platform2',
+		category: 'Web Application',
+		img: new URL('../assets/images/web-project-2.jpg', import.meta.url).href ,
+	},
+	{
+		id: 2,
+		title: 'Phoenix Digital Agency',
+		category: 'Mobile Application',
+		img: new URL('../assets/images/mobile-project-2.jpg', import.meta.url).href,
+	},
+	{
+		id: 3,
+		title: 'Project Management UI',
+		category: 'UI/UX Design',
+		img: new URL('../assets/images/ui-project-1.jpg', import.meta.url).href,
+	},
+	{
+		id: 4,
+		title: 'Cloud Storage Platform',
+		category: 'UI/UX Design',
+		img: new URL('../assets/images/ui-project-2.jpg', import.meta.url).href,
+	},
+	{
+		id: 5,
+		title: 'React Social App',
+		category: 'Mobile Application',
+		img: new URL('../assets/images/mobile-project-1.jpg', import.meta.url).href,
+	},
+	{
+		id: 6,
+		title: 'Apple Design System',
+		category: 'Web Application',
+		img: new URL('../assets/images/web-project-1.jpg', import.meta.url).href,
+	},
+];
+
+export default projects;
