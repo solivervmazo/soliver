c6f72f8694736b2162c03dfe2ba5af04242e9174
LAPTOP-Q8PH4MAIPC <solivervmazo.jobs@gmail.com>
2023-07-12 09:29:41 +0800

    'Updating'

---
+++ assets/Hello-f9cd1e76.js
@@ -0,0 +1,4 @@
+import{o as e,c as o,q as t}from"./index-8fe9f8eb.js";const n={class:"markdown-body"},l=t(`<pre class="hljs"><code><span class="hljs-built_in">print</span>(<span class="hljs-string">&#39;Hello World&#39;</span>)
+</code></pre><pre><code>Hello World
+</code></pre><pre class="hljs"><code><span class="hljs-built_in">print</span>(<span class="hljs-string">&#39;Hello World2&#39;</span>)
+</code></pre>`,3),a=[l],i={__name:"Hello",setup(c,{expose:s}){return s({frontmatter:{}}),(p,d)=>(e(),o("div",n,a))}};export{i as default};
