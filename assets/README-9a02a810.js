4b181ddda9c8056b67b7cdbfed2d8f022176be26
LAPTOP-Q8PH4MAIPC <solivervmazo.jobs@gmail.com>
2023-07-15 01:12:06 +0800

    'Updating'

--- assets/README-9a02a810.js
+++ assets/README-9a02a810.js
@@ -1,4 +1,4 @@
-import{K as a,o as n,c as l,L as p}from"./index-5ed2094b.js";const o={class:"markdown-body"},r=p(`<h2>SQL</h2><p>Data source: <a href="http://">https://www.kaggle.com/datasets/minemartin/london-airbnb-listings-march-2023</a></p><pre class="hljs"><code><span class="hljs-comment">/*
+import{K as a,o as n,c as l,L as p}from"./index-3c27e980.js";const o={class:"markdown-body"},r=p(`<h2>SQL</h2><p>Data source: <a href="http://">https://www.kaggle.com/datasets/minemartin/london-airbnb-listings-march-2023</a></p><pre class="hljs"><code><span class="hljs-comment">/*
 Cleaning Data in SQL Queries
 Dataset:https://www.kaggle.com/datasets/minemartin/london-airbnb-listings-march-2023
 */</span>
